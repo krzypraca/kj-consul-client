8bea8dda5591dc109126e16c357180af58b3b439
kj <krzypraca@gmx.com>
2016-04-05 14:50:20 -0700

    First commit

---
+++ src/main/java/com/kj/KjConsulClientApplication.java
@@ -0,0 +1,13 @@
+package com.kj;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class KjConsulClientApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(KjConsulClientApplication.class, args);
+	}
+}
